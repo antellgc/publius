ba2aec9eea2013f80f9b70a78984a23b40a0622f
antellgc <antellgc@gmail.com>
2019-01-03 23:38:31 -0500

    edited scraping script

--- scripts/scrape_federalist_papers.py
+++ scripts/scrape_federalist_papers.py
@@ -3,49 +3,65 @@ from bs4 import BeautifulSoup
 import pandas as pd
 pd.set_option('chained_assignment',None)
 
-# queries the main url
+# global variables
 url = 'https://www.congress.gov/resources/display/content/The+Federalist+Papers'
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+author_names = ['Madison','Hamilton','Jay']
 
-# parses the text information
-text_block = soup.find_all("div", class_="wiki-content")[0].get_text()
-all_texts = []
-n = 1
-for string in text_block.split('|| Federalist No.')[1:]:
-    string = string.replace(u'\xa0', u' ')
-    string = '|| Federalist No.' + string
-    string = string.split('To the People of the State of New York:')[1]
-    string = string.split('↑ Back to Top')[0]
-    string = string.split('PUBLIUS')[0]
-    all_texts.append(string)
-    n += 1
+def get_soup_from_url(url):
+    """Query a given url and return the page content as BeautifulSoup object."""
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
 
-# extracts table information
-count = 0
-row = []
-all_rows = []
-for i in soup.find_all("td", class_="confluenceTd"):
-    count += 1
-    if count%5 != 0:
-        row.append(i.get_text())
-    else:
-        row.append(i.get_text())
-        all_rows.append(row)
-        row = []
+def parse_federalist_text(soup):
+    """Extract the text information based on known structure of Federalist papers."""
+    text_block = soup.find_all("div", class_="wiki-content")[0].get_text()
+    all_texts = []
+    n = 1
+    for string in text_block.split('|| Federalist No.')[1:]:
+        string = string.replace(u'\xa0', u' ')
+        string = '|| Federalist No.' + string
+        string = string.split('To the People of the State of New York:')[1]
+        string = string.split('↑ Back to Top')[0]
+        string = string.split('PUBLIUS')[0]
+        all_texts.append(string)
+        n += 1
+    return all_texts
 
-# stores info in dataframe
-df = pd.DataFrame(all_rows)
-df.columns = ['No.','Title','Author','Publication','Date']
-# adds text and text length columns
-df.loc[:,'Text'] = all_texts
-df = df[['No.','Title','Author','Text']]
-df.loc[:,'Length'] = df['Text'].apply(len)
+def extract_table_data(soup):
+    """Extract table information and return as list object."""
+    count = 0
+    row = []
+    all_rows = []
+    for i in soup.find_all("td", class_="confluenceTd"):
+        count += 1
+        if count%5 != 0:
+            row.append(i.get_text())
+        else:
+            row.append(i.get_text())
+            all_rows.append(row)
+            row = []
+    return all_rows
+
+def store_paper_data_in_dataframe(all_rows, all_texts):
+    """Store table data and text data into a single pandas dataframe."""
+    federalist_dataframe = pd.DataFrame(all_rows)
+    federalist_dataframe.columns = ['No.','Title','Author','Publication','Date']
+    # adds text and text length columns
+    federalist_dataframe.loc[:,'Text'] = all_texts
+    federalist_dataframe = federalist_dataframe[['No.','Title','Author','Text']]
+    federalist_dataframe.loc[:,'Length'] = federalist_dataframe['Text'].apply(len)
+    return federalist_dataframe
+
+def select_papers_by_author(federalist_dataframe,author_name):
+    """Provide name and return dataframe containing only this author."""
+    author_dataframe = federalist_dataframe[federalist_dataframe['Author'] == author_name].reset_index().drop(['index'], axis=1)
+    return author_dataframe
 
 # selects according to author
-df_ham = df[df['Author'] == 'Hamilton'].reset_index().drop(['index'], axis=1)
-df_mad = df[df['Author'] == 'Madison'].reset_index().drop(['index'], axis=1)
-df_jay = df[df['Author'] == 'Jay'].reset_index().drop(['index'], axis=1)
+#df_ham = df[df['Author'] == 'Hamilton'].reset_index().drop(['index'], axis=1)
+#df_mad = df[df['Author'] == 'Madison'].reset_index().drop(['index'], axis=1)
+#df_jay = df[df['Author'] == 'Jay'].reset_index().drop(['index'], axis=1)
 
 # saves to raw_data directory
 df_ham .to_csv('../raw_data/hamilton.csv', index=False)
