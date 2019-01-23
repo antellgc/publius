94b58cb34046e31f5114bc21bb32fe98b1316c11
antellgc <antellgc@gmail.com>
2019-01-23 12:08:46 -0500

    updated image outputs

--- starter_app.py
+++ starter_app.py
@@ -15,8 +15,7 @@ df_fed_para = pd.read_csv("./processed_data/fed_paragraphs.csv")
 df_test_docs['Indicator Name'] = "No. " + df_test_docs['No.'].map(str) + " - " + df_test_docs['Title']
 available_indicators = df_test_docs['Indicator Name'].unique()
 
-### app
-
+### APP LAYOUT
 app = dash.Dash(__name__,
     static_folder='static',
     external_stylesheets=external_stylesheets)
@@ -25,9 +24,6 @@ app.layout = html.Div(children=[
 
     html.H1(children="What's your name, man?",
         style = {'font-size':'4em'}),
-
-    html.Img(src="static/constitution.png",
-        style = {'max-width':600}),
 
     html.H3(children="Uncovering the authorship of the anonymously-written \
         Federalist Papers with machine learning"),
@@ -40,31 +36,75 @@ app.layout = html.Div(children=[
         placeholder="Select a Federalist Paper"
         ),
 
-    html.H1(id='image-output-id'),
+    html.Br(),
+
+    html.Img(id="placeholder-image-id", style={'max-width':600}),
+
+    html.Div([
+        html.Div([
+            html.H2(id='true-author-name'),
+            html.Img(id='true-image-id', style={'max-width':600})
+        ], style={'width': '49%', 'display': 'inline-block'}),
+    
+        html.Div([
+            html.H2(id='predicted-author-name'),
+            html.Img(id='predicted-image-id', style={'max-width':600})
+        ], style={'width': '49%', 'display': 'inline-block'})
+    ]),
 
     html.H1(id='model-output-id'),
 
-    html.Blockquote(id='paragraph-output-id', style = {'textAlign':'left'})], 
-    style={'textAlign': 'center'})
+    html.Blockquote(
+        id ='paragraph-output-id',
+        style = {'textAlign':'left'}
+        )
+    ], 
+    style = {'textAlign': 'center'})
 
-##### outputs
+##### APP OUTPUTS/CALLBACKS
 
-### IMAGE OUTPUT
+#PLACEHOLDER IMAGE
 @app.callback(
-    Output(component_id='image-output-id', component_property='children'),
+    Output(component_id='placeholder-image-id', component_property='src'),
     [Input(component_id='dropdown-id', component_property='value')]
 )
-def update_author_output(value):
+def update_true_image(value):
+    if value in list(df_test_docs['Indicator Name']):
+        return None
+    else:
+        src_name = "static/constitution.png"
+        return src_name
+
+#TRUE AUTHOR NAME
+@app.callback(
+    Output(component_id='true-author-name', component_property='children'),
+    [Input(component_id='dropdown-id', component_property='value')]
+)
+def update_true_name(value):
     if value in list(df_test_docs['Indicator Name']):
         mask1 = df_test_docs['Indicator Name'] == value
-        author = list(df_test_docs[mask1]['Author'])[0].lower()
-        return author
+        author_name = list(df_test_docs[mask1]['Author'])[0]
+        return "True Author: " + author_name
     else:
         return None
 
-### MODEL PREDICTION
+#TRUE IMAGE
 @app.callback(
-    Output(component_id='model-output-id', component_property='children'),
+    Output(component_id='true-image-id', component_property='src'),
+    [Input(component_id='dropdown-id', component_property='value')]
+)
+def update_true_image(value):
+    if value in list(df_test_docs['Indicator Name']):
+        mask1 = df_test_docs['Indicator Name'] == value
+        author = list(df_test_docs[mask1]['Author'])[0].lower()
+        src_name = "static/" + author + ".png"
+        return src_name
+    else:
+        return None
+
+#MODEL PREDICTION
+@app.callback(
+    Output(component_id='predicted-author-name', component_property='children'),
     [Input(component_id='dropdown-id', component_property='value')]
 )
 def update_model_output(value):
@@ -73,7 +113,7 @@ def update_model_output(value):
     else:
         return None
 
-#### PARAGRAPH TEXT
+#PARAGRAPH TEXT
 @app.callback(
     Output(component_id='paragraph-output-id', component_property='children'),
     [Input(component_id='dropdown-id', component_property='value')]
