a5bcf267d75994c2cc122f34a9efae119429439a
antellgc <antellgc@gmail.com>
2019-01-22 22:12:54 -0500

    starter app dropdown menu

--- starter_app.py
+++ starter_app.py
@@ -15,14 +15,7 @@ df_fed_para = pd.read_csv("./processed_data/fed_paragraphs.csv")
 df_test_docs['Indicator Name'] = "No. " + df_test_docs['No.'].map(str) + " - " + df_test_docs['Title']
 available_indicators = df_test_docs['Indicator Name'].unique()
 
-# generating paragraphs functin
-def generate_paragraphs(dataframe, no):
-    df_show = dataframe[dataframe['No.'] == no]
-    p_list = list(df_show['ParaText'])
-    return html.Div([html.P(paragraph) for paragraph in p_list])
-
-## selecting a paper (#71 as an example)
-no = 78 # temporary
+### app
 
 app = dash.Dash(__name__,
     static_folder='static',
@@ -47,23 +40,53 @@ app.layout = html.Div(children=[
         placeholder="Select a Federalist Paper"
         ),
 
-    html.H3(id='output-id'),
+    html.H1(id='image-output-id'),
 
-    #html.Blockquote(generate_paragraphs(df_fed_para, no),
-    #    style = {'textAlign':'left'})
-    ], 
+    html.H1(id='model-output-id'),
+
+    html.Blockquote(id='paragraph-output-id', style = {'textAlign':'left'})], 
     style={'textAlign': 'center'})
 
+##### outputs
+
+### IMAGE OUTPUT
 @app.callback(
-    Output(component_id='output-id', component_property='children'),
+    Output(component_id='image-output-id', component_property='children'),
     [Input(component_id='dropdown-id', component_property='value')]
 )
-def update_output_div(value):
-    mask = df_test_docs['Indicator Name'] == value
-    #title = df_test_docs[mask]['Title']
-    no = df_test_docs[mask]['No.']
-    return no
-    #return 'You have selected {}'.format(value)
+def update_author_output(value):
+    if value in list(df_test_docs['Indicator Name']):
+        mask1 = df_test_docs['Indicator Name'] == value
+        author = list(df_test_docs[mask1]['Author'])[0].lower()
+        return author
+    else:
+        return None
+
+### MODEL PREDICTION
+@app.callback(
+    Output(component_id='model-output-id', component_property='children'),
+    [Input(component_id='dropdown-id', component_property='value')]
+)
+def update_model_output(value):
+    if value in list(df_test_docs['Indicator Name']):
+        return value
+    else:
+        return None
+
+#### PARAGRAPH TEXT
+@app.callback(
+    Output(component_id='paragraph-output-id', component_property='children'),
+    [Input(component_id='dropdown-id', component_property='value')]
+)
+def update_text_output(value):
+    if value in list(df_test_docs['Indicator Name']):
+        mask1 = df_test_docs['Indicator Name'] == value
+        title = list(df_test_docs[mask1]['Title'])[0]
+        mask2 = df_fed_para['Title'] == title
+        p_list = list(df_fed_para[mask2]['ParaText'])
+        return [html.P(paragraph) for paragraph in p_list]
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run_server(debug=True)
